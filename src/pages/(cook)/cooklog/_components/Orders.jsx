19a04399d86baf06b6e53a10edb29a7a2d8e2ca6
MehmetGulbahar <121450823+MehmetGulbahar@users.noreply.github.com>
2024-04-21 21:57:57 +0300

    Cook order confirmation has been updated.

--- src/pages/(cook)/cooklog/_components/Orders.jsx
+++ src/pages/(cook)/cooklog/_components/Orders.jsx
@@ -1,43 +1,128 @@
-import React from "react";
+import { useEffect, useState } from "react";
 
-export default function Orders(){
-    return (
-      <div className="grid h-20 flex-grow  bg-base-300 place-items-center">
-        <table className="min-w-full divide-y-2 divide-gray-200 bg-white text-sm">
-          <thead className="ltr:text-left rtl:text-right">
-            <tr>
-              <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
-                Order Id
-              </th>
-              <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
-                Dish
-              </th>
-              <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
-                Quantity
-              </th>
-              <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900"></th>
-              <th className="px-4 py-2"></th>
-            </tr>
-          </thead>
-          <tbody className="divide-y divide-gray-200">
-            <tr>
+export default function Orders() {
+  const token = localStorage.getItem("token");
+  const [orders, setOrders] = useState([]);
+  const [isAccepted, setIsAccepted] = useState(false);
+
+  const closeModal = () => {
+    setIsAccepted(false);
+  };
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/v1/order/viewAll/cook", {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setOrders(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
+  const handleAccept = (orderId) => {
+    fetch(
+      `http://localhost:8080/api/v1/order/confirm_cook?id=${orderId}&accept=true`,
+      {
+        method: "POST",
+        credentials: "include",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    )
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        closeModal();
+        setIsAccepted(true);
+        deleteOperation(orderId);
+      })
+      .catch((error) => {
+        console.error("Error accepting order:", error);
+      });
+  };
+
+  const handleReject = (orderId) => {
+    fetch(
+      `http://localhost:8080/api/v1/order/confirm_cook?id=${orderId}&accept=false`,
+      {
+        method: "POST",
+        credentials: "include",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    )
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        closeModal();
+      })
+      .catch((error) => {
+        console.error("Error rejecting order:", error);
+      });
+  };
+
+  const deleteOperation = (orderId) => {
+    const updatedOrders = orders.filter((order) => order.order.id !== orderId);
+    setOrders(updatedOrders);
+  };
+
+
+  return (
+    <div className="grid h-20 flex-grow bg-base-300 place-items-center">
+      <table className="min-w-full divide-y-2 divide-gray-200 bg-white text-sm">
+        <thead className="ltr:text-left rtl:text-right">
+          <tr>
+            <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+              Order Id
+            </th>
+            <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+              Dish
+            </th>
+            <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+              Quantity
+            </th>
+            <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900"></th>
+            <th className="px-4 py-2"></th>
+          </tr>
+        </thead>
+        <tbody className="divide-y divide-gray-200">
+          {orders.map((order) => (
+            <tr key={order.order.id}>
               <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
-                1
+                {order.order.id}
               </td>
               <td className="whitespace-nowrap px-4 py-2 text-gray-700">
-                Haydari
+                {order.plates.map((plate) => plate.food).join(", ")}
               </td>
               <td className="whitespace-nowrap px-4 py-2 text-gray-700">
-                2
+                {order.order.totalPrice}
               </td>
               <td className="whitespace-nowrap px-4 py-2">
                 <a
+                  onClick={() => handleAccept(order.order.id)}
                   href="#"
-                  className=" mr-1 inline-block rounded bg-green-600 px-4 py-2 text-xs font-medium text-white hover:bg-green-700"
+                  className="mr-1 inline-block rounded bg-green-600 px-4 py-2 text-xs font-medium text-white hover:bg-green-700"
                 >
                   Accept
                 </a>
                 <a
+                  onClick={() => handleReject(order.order.id)}
                   href="#"
                   className="inline-block rounded bg-red-600 px-4 py-2 text-xs font-medium text-white hover:bg-red-700"
                 >
@@ -47,7 +132,7 @@ export default function Orders(){
               <td className="whitespace-nowrap px-4 py-2">
                 <div
                   className="tooltip tooltip-right"
-                  data-tip="Tuzsuz"
+                  data-tip={order.plates.map((plate) => plate.note).join(", ")}
                 >
                   <a className="inline-block rounded bg-indigo-600 px-4 py-2 text-xs font-medium text-white hover:bg-indigo-700">
                     View
@@ -55,43 +140,9 @@ export default function Orders(){
                 </div>
               </td>
             </tr>
-            <tr>
-              <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
-                1
-              </td>
-              <td className="whitespace-nowrap px-4 py-2 text-gray-700">
-                Haydari
-              </td>
-              <td className="whitespace-nowrap px-4 py-2 text-gray-700">
-                2
-              </td>
-              <td className="whitespace-nowrap px-4 py-2">
-                <a
-                  href="#"
-                  className=" mr-1 inline-block rounded bg-green-600 px-4 py-2 text-xs font-medium text-white hover:bg-green-700"
-                >
-                  Accept
-                </a>
-                <a
-                  href="#"
-                  className="inline-block rounded bg-red-600 px-4 py-2 text-xs font-medium text-white hover:bg-red-700"
-                >
-                  Decline
-                </a>
-              </td>
-              <td className="whitespace-nowrap px-4 py-2">
-                <div
-                  className="tooltip tooltip-right"
-                  data-tip="Tuzsuz, biberli"
-                >
-                  <a className="inline-block rounded bg-indigo-600 px-4 py-2 text-xs font-medium text-white hover:bg-indigo-700">
-                    View
-                  </a>
-                </div>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    );
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
--- src/pages/(cook)/cooklog/_components/Todo.jsx
+++ src/pages/(cook)/cooklog/_components/Todo.jsx
@@ -1,47 +1,79 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Timer from "./Timer";
-export default function Todo(){
-    return(
-        <div className="grid h-full flex-grow  bg-base-300 place-items-center">
-        <table className="min-w-full divide-y-2 divide-gray-200 bg-white text-sm">
-          <thead className="ltr:text-left rtl:text-right">
-            <tr>
-              <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
-                Order Id
-              </th>
-              <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
-                Dish
-              </th>
-              <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
-                Quantity
-              </th>
-              <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900"></th>
-              <th className="px-4 py-2"></th>
-            </tr>
-          </thead>
-          <tbody className="divide-y divide-gray-200">
-            <tr>
-              <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
-                1
-              </td>
-              <td className="whitespace-nowrap px-4 py-2 text-gray-700">
-                Haydari
-              </td>
-              <td className="whitespace-nowrap px-4 py-2 text-gray-700">
-                2
-              </td>
-              <td className="whitespace-nowrap col-span-2 px-4 py-2">
-                
-              <Timer time={5}/>
-             
-              </td>
-             
-            </tr>
-           
-          </tbody>
-        </table>
-</div>
 
-       
-    )
+export default function Todo() {
+  const token = localStorage.getItem("token");
+  const [orders, setOrders] = useState([]);
+  const [isAccepted, setIsAccepted] = useState(false);
+
+  const openModal = () => {
+    setIsAccepted(true);
+  };
+
+  const closeModal = () => {
+    setIsAccepted(false);
+  };
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/v1/order/viewAll/cook", {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setOrders(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
+  return (
+    <div className="grid h-full flex-grow bg-base-300 place-items-center">
+      <table className="min-w-full divide-y-2 divide-gray-200 bg-white text-sm">
+        <thead className="ltr:text-left rtl:text-right">
+          <tr>
+            <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+              Order Id
+            </th>
+            <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+              Dish
+            </th>
+            <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+              Quantity
+            </th>
+            <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900"></th>
+            <th className="px-4 py-2"></th>
+          </tr>
+        </thead>
+        <tbody className="divide-y divide-gray-200">
+          {orders.map((order) => (
+            <tr key={order.order.id}>
+              <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                {order.order.id}
+              </td>
+              <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                {order.plates.map((plate) => plate.food).join(", ")}
+              </td>
+              <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                Quantity
+              </td>
+              <td className="whitespace-nowrap px-4 py-2">
+                <Timer time={5} />
+              </td>
+              <td className="whitespace-nowrap px-4 py-2"></td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
